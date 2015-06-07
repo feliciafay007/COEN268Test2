8584bbf2cc81cf9e4cdfdc1e8c6fc2e12f865a63
feliciafay007 <feliciafay@gmail.com>
2015-06-07 15:04:39 -0700

    CRUD operation on a database done

--- sqlitedemo1/src/main/java/test1/ui/android/felicia/sqlitedemo1/MainActivity.java
+++ sqlitedemo1/src/main/java/test1/ui/android/felicia/sqlitedemo1/MainActivity.java
@@ -2,6 +2,7 @@ package test1.ui.android.felicia.sqlitedemo1;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
@@ -11,12 +12,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends Activity {
     private Button buttonShow;
     private Button buttonAdd ;
     private Button buttonDelete;
+    private Button buttonUpdate;
+    private EditText editTextName;
+    private EditText editTextDes;
+    private EditText editTextPath;
+    private EditText editTextUpdate;
+    private EditText editTextDelete;
+    private TextView textViewShow;
     SQLiteDatabase db;
 
 
@@ -27,6 +39,14 @@ public class MainActivity extends Activity {
         buttonShow = (Button) findViewById(R.id.buttonShow);
         buttonAdd = (Button) findViewById(R.id.buttonAdd);
         buttonDelete = (Button) findViewById(R.id.buttonDelete);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextDes = (EditText) findViewById(R.id.editTextDes);
+        editTextPath= (EditText) findViewById(R.id.editTextPath);
+        editTextUpdate= (EditText) findViewById(R.id.editTextUpdate);
+        editTextDelete = (EditText) findViewById(R.id.editTextDelete);
+        textViewShow = (TextView)  findViewById(R.id.textViewShow);
+        buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+
         db = new ZooDbHelper(getApplicationContext()).getWritableDatabase();
 
         buttonShow.setOnClickListener(new View.OnClickListener() {
@@ -41,26 +61,69 @@ public class MainActivity extends Activity {
                         ZooDbHelper.DESCRIPTION_COLUMN, ZooDbHelper.FILE_PATH_COLUMN};
                 Cursor cursor = db.query(ZooDbHelper.DATABASE_TABLE, resultColumns, where,
                         whereArgs, groupBy, having, order);
+                StringBuilder sb = new StringBuilder();
                 while (cursor.moveToNext()) {
                     int id = cursor.getInt(0);
                     String name = cursor.getString(1);
                     String description = cursor.getString(2);
                     String filepath = cursor.getString(3);
-                    Log.d("ZOO", String.format("%s,%s,%s,%s", id, name, description,
-                            filepath));
+                    sb.append(id).append(", ").append(name).append(", ").append(description);
+                   // Log.d("ZOO", String.format("%s,%s,%s,%s", id, name, description,filepath));
                 }
+                sb.append("\n");
+                textViewShow.setText(sb.toString());
             }
         });
 
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String currentName = editTextName.getText().toString();
+                String currentDes= editTextDes.getText().toString();
+                String currentPath= editTextPath.getText().toString();
                 SQLiteDatabase db = new ZooDbHelper(getApplicationContext()).getWritableDatabase();
                 ContentValues newValues = new ContentValues();
-                newValues.put(ZooDbHelper.NAME_COLUMN, "alpaca");
-                newValues.put(ZooDbHelper.DESCRIPTION_COLUMN, "An alpaca looks like a lama.");
-                newValues.put(ZooDbHelper.FILE_PATH_COLUMN, "/storage/alpaca.png");
+                if (currentName == null ||
+                    currentDes == null ||
+                    currentPath == null ||
+                    currentName.isEmpty() ||
+                    currentDes.isEmpty() ||
+                    currentPath.isEmpty())
+                {
+                    newValues.put(ZooDbHelper.NAME_COLUMN, "alpaca");
+                    newValues.put(ZooDbHelper.DESCRIPTION_COLUMN, "An alpaca looks like a lama.");
+                    newValues.put(ZooDbHelper.FILE_PATH_COLUMN, "/storage/alpaca.png");
+                } else {
+                    newValues.put(ZooDbHelper.NAME_COLUMN, currentName);
+                    newValues.put(ZooDbHelper.DESCRIPTION_COLUMN, currentDes);
+                    newValues.put(ZooDbHelper.FILE_PATH_COLUMN, currentPath);
+                }
                 db.insert(ZooDbHelper.DATABASE_TABLE, null, newValues);
+            }
+        });
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idInEditText = editTextUpdate.getText().toString();
+                String whereClause = ZooDbHelper.ID_COLUMN + "= ?";
+                String[] whereArgs = {idInEditText};
+                ContentValues newValues = new ContentValues();
+                newValues.put(ZooDbHelper.NAME_COLUMN, "alpaca");
+                newValues.put(ZooDbHelper.DESCRIPTION_COLUMN, "An alpaca is ugly.");
+                newValues.put(ZooDbHelper.FILE_PATH_COLUMN, "/storage/alpaca.png");
+                db.update(ZooDbHelper.DATABASE_TABLE, newValues, whereClause, whereArgs);
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String whereClause = ZooDbHelper.ID_COLUMN + "=?";
+                //String[] whereArgs = {"4"};
+                String idInEditText = editTextDelete.getText().toString();
+                String[] whereArgs = {idInEditText};
+                db.delete(ZooDbHelper.DATABASE_TABLE, whereClause, whereArgs);
             }
         });
     }
@@ -82,8 +145,13 @@ public class MainActivity extends Activity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.card_view) {
+            Intent intent = new Intent(MainActivity.this, CardDisplayActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.CRUD_view) {
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
